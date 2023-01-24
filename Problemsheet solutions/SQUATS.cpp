c5cdd9b036caf9703834cedb20476cf85215687c
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-01-25 01:33:11 +0600

    updated

---
+++ Problemsheet solutions/SQUATS.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long x;
+        cin>>x;
+
+        cout<<x*15<<endl;
+    }
+
+
+
+    return 0;
+}
