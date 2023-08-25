c63c6389c725d789aad5f70a7c25a3d62a4e9329
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-08-26 01:50:31 +0600

    done

---
+++ Specefic/REACH_HOME.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin >>t;
+    while(t--)
+    {
+
+        int f,d;
+        cin >>f>>d;
+        int p=f*5;
+
+        if(d<=p) cout<< "YES"<<endl;
+        else cout<< "NO"<<endl;
+
+    }
+    return 0;
+}
