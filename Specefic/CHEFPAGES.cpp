f9fe67914376dda57b01408d73105c5fd3d7ed4c
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-08-31 15:08:41 +0600

    CHEFPAGES

---
+++ Specefic/CHEFPAGES.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a, b;
+	cin >>a >>b;
+
+	if(a==1 && b==1)
+	{
+	    cout<< "https://discuss.codechef.com" <<endl;
+	}
+	else if(a==1 && b==0)
+	{
+	    cout<< "https://www.codechef.com/contests"<<endl;
+	}
+	else
+	{
+	    cout<< "https://www.codechef.com/practice"<<endl;
+	}
+	return 0;
+}
+
